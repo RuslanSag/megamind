e42f5183fe80c977f2c3a676d34869323e1660c9
rusiksag <coworkingruslan@gmail.com>
2023-12-17 17:02:09 +0500

    last update

--- calculation.php
+++ calculation.php
@@ -1,8 +1,7 @@
 <?php
-include './header.php';
+include 'header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +14,8 @@ include './header.php';
       margin-bottom: 20px;
     }
 
-    th, td {
+    th,
+    td {
       border: 1px solid #ddd;
       padding: 8px;
       text-align: left;
@@ -26,7 +26,9 @@ include './header.php';
     }
   </style>
 </head>
+
 <body>
+
   <section class="cal_table">
     <table>
       <thead>
@@ -44,89 +46,147 @@ include './header.php';
           <td><input type="text" id="inputScore" oninput="calculatePoints()"></td>
           <td id="result"></td>
         </tr>
-  
+
         <tr>
           <td>5 daqiqalik Yuz va ismlar</td>
           <td>names/95*1000</td>
           <td><input type="text" id="inputScore2" oninput="calculatePoints2()"></td>
           <td id="result2"></td>
         </tr>
-  
+
         <tr>
           <td>5 daqiqalik Raqamlar</td>
           <td>digits/649*1000</td>
           <td><input type="text" id="inputScore3" oninput="calculatePoints3()"></td>
           <td id="result3"></td>
         </tr>
-  
+
+        <tr>
+          <td>5 daqiqalik So’zlar</td>
+          <td>words/153*1000</td>
+          <td><input type="text" id="inputScore4" oninput="calculatePoints4()"></td>
+          <td id="result4"></td>
+        </tr>
+
+        <tr>
+          <td>10 daqiqalik Kartalar</td>
+          <td>cards/589*1000</td>
+          <td><input type="text" id="inputScore5" oninput="calculatePoints5()"></td>
+          <td id="result5"></td>
+        </tr>
+
+        <tr>
+          <td>15 daqiqalik Raqamlar</td>
+          <td>digits/1385*1000</td>
+          <td><input type="text" id="inputScore6" oninput="calculatePoints6()"></td>
+          <td id="result6"></td>
+        </tr>
+
+        <tr>
+          <td>5 daqiqalik Rasmlar</td>
+          <td>images/567*1000</td>
+          <td><input type="text" id="inputScore7" oninput="calculatePoints7()"></td>
+          <td id="result7"></td>
+        </tr>
+
+
+        <tr>
+          <td>5 daqiqalik Tarixiy sanalar</td>
+          <td>dates/142*1000</td>
+          <td><input type="text" id="inputScore8" oninput="calculatePoints8()"></td>
+          <td id="result8"></td>
+        </tr>
+
+        <tr>
+          <td>Ovozli Raqamlar</td>
+          <td>&radic;(digits)*47,3</td>
+          <td><input type="text" id="inputScore9" oninput="calculatePoints9()"></td>
+          <td id="result9"></td>
+        </tr>
+
         <tr>
           <td></td>
           <td></td>
-          <td></td>
+          <td><b>Overall</b></td>
           <td id="overall"></td>
         </tr>
       </tbody>
     </table>
-
   </section>
-
 
   <script>
     function calculatePoints() {
-      // Get the input value
-      var inputScore = document.getElementById('inputScore').value;
-
-      // Perform the calculation
-      var result = (inputScore / 1550) * 1000;
-
-      // Update the result in the corresponding <td>
-      document.getElementById('result').textContent = result.toFixed(2);
-      
-      // Recalculate the overall result
-      calculateOverall();
+      updateResult('result', 'inputScore', 1550);
     }
 
     function calculatePoints2() {
-      // Get the input value
-      var inputScore = document.getElementById('inputScore2').value;
-
-      // Perform the calculation
-      var result = (inputScore / 95) * 1000;
-
-      // Update the result in the corresponding <td>
-      document.getElementById('result2').textContent = result.toFixed(2);
-      
-      // Recalculate the overall result
-      calculateOverall();
+      updateResult('result2', 'inputScore2', 95);
     }
 
     function calculatePoints3() {
-      // Get the input value
-      var inputScore = document.getElementById('inputScore3').value;
+      updateResult('result3', 'inputScore3', 649);
+    }
 
-      // Perform the calculation
-      var result = (inputScore / 649) * 1000;
+    function calculatePoints4() {
+      updateResult('result4', 'inputScore4', 153);
+    }
 
-      // Update the result in the corresponding <td>
-      document.getElementById('result3').textContent = result.toFixed(2);
-      
-      // Recalculate the overall result
+    function calculatePoints5() {
+      updateResult('result5', 'inputScore5', 589);
+    }
+
+    function calculatePoints6() {
+      updateResult('result6', 'inputScore6', 1385);
+    }
+
+    function calculatePoints7() {
+      updateResult('result7', 'inputScore7', 567);
+    }
+
+    function calculatePoints8() {
+      updateResult('result8', 'inputScore8', 142);
+    }
+
+    function calculatePoints9() {
+      var inputScore = document.getElementById('inputScore9').value;
+      var result = Math.sqrt(inputScore) * 47.3;
+      document.getElementById('result9').textContent = result ? result.toFixed(2) : "";
       calculateOverall();
     }
 
+    function updateResult(resultId, inputId, divisor) {
+      var inputScore = document.getElementById(inputId).value;
+      var result = (inputScore !== "") ? (inputScore / divisor) * 1000 : "";
+      document.getElementById(resultId).textContent = result ? result.toFixed(2) : "";
+      calculateOverall();
+    }
+
+
     function calculateOverall() {
-      // Get individual results
-      var result1 = parseFloat(document.getElementById('result').textContent);
-      var result2 = parseFloat(document.getElementById('result2').textContent);
-      var result3 = parseFloat(document.getElementById('result3').textContent);
+      var results = [
+        'result', 'result2', 'result3', 'result4',
+        'result5', 'result6', 'result7', 'result8', 'result9'
+      ];
+      var overallResult = 0;
+      var count = 0;
 
-      // Calculate overall result
-      var overallResult = (result1 + result2 + result3) / 3;
+      for (var i = 0; i < results.length; i++) {
+        var result = parseFloat(document.getElementById(results[i]).textContent) || 0;
+        if (result !== 0) {
+          overallResult += result;
+          count++;
+        }
+      }
 
-      // Update the overall result in the corresponding <td>
+      overallResult = (count !== 0) ? overallResult / count : 0;
+
       document.getElementById('overall').textContent = overallResult.toFixed(2);
     }
   </script>
 
+
+
+
 </body>
+
 </html>
